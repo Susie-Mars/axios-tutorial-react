1c30f60c6e6111de6c272b31f76100fb156422be
Susie-Mars <marsnarsbars@gmail.com>
2023-01-16 09:03:15 -0700

    axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import Title from './components/Title';
+import Title from "./components/Title";
+import Setup from "./examples/5-custom-instance";
+import "./axios/global";
 function App() {
   return (
     <main>
       <Title />
+      <Setup />
     </main>
   );
 }
